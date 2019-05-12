f333681b0c88d9adeb74dadb43fae53ea0ccf5e4
jiangxiaoqiang <jiangtingqiang@gmail.com>
2019-05-12 15:14:53 +0800

    initial

---
+++ feign/src/main/java/com/cloud/eureka/feign/FeignApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.eureka.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+
+}
